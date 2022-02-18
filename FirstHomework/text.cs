8f7b36113f9deef1878bcdf85f99d5c44e3bf9dd
avbprogramming <avb.programming@gmail.com>
2022-02-18 20:34:32 +0300

    avb_hw_v1

---
+++ FirstHomework/text.cs
@@ -0,0 +1,10 @@
+namespace FirstHomework;
+
+public class text
+{
+    public static void firstText()
+    {
+        Console.WriteLine("У одних родителей мальчик был. Звали его дядя Фёдор.");
+        Console.WriteLine("Press Enter when you ready, and Enter when you finish ");
+    }
+}
